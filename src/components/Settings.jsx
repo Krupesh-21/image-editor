8d89fcce176a24b975887234e288e8e28a09dfd6
Krupesh Joshi <krupeshjoshi21@gmail.com>
2024-01-16 16:02:09 +0530

    apply filters and other settings using canvas & javascript

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,6 +6,8 @@ const options = [
   { name: "brightness", max: 200 },
   { name: "saturation", max: 200 },
   { name: "inversion", max: 100 },
+  { name: "exposure", max: 100 },
+  { name: "contrast", max: 100 },
 ];
 
 const Settings = () => {
